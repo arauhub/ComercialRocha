8110312a8f24e9323100c2e481d25f67a7d270b3
arahub <cllaappp@gmail.com>
2026-04-13 19:07:38 -0300

    v2.7

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker Corrigido - Comercial Rocha
 // Versão corrigida que resolve o erro de chrome-extension
 
-const CACHE_NAME = 'comercial-rocha-cache-v27';
+const CACHE_NAME = 'comercial-rocha-cache-v28';
 
 const urlsToCache = [
   './index.html'
